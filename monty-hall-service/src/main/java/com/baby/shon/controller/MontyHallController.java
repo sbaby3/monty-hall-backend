b07f31b18faaba65680c3ac4ec7e1787e121193a
sbaby3 <babyshon92@gmail.com>
2023-08-27 22:49:53 -0400

    updated for CORS

--- monty-hall-service/src/main/java/com/baby/shon/controller/MontyHallController.java
+++ monty-hall-service/src/main/java/com/baby/shon/controller/MontyHallController.java
@@ -3,10 +3,12 @@ package com.baby.shon.controller;
 import com.baby.shon.service.GameService;
 import com.baby.shon.vo.GameResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class MontyHallController {
 
